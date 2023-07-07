06193aabfe6230cdb6c0c034e3e0b07e12b51a49
faulinaindri <92627115+Lilitridesma@users.noreply.github.com>
2023-07-07 14:45:36 +0700

    Forex

--- app/src/main/java/com/kania201103722/login/MenuActivity.java
+++ app/src/main/java/com/kania201103722/login/MenuActivity.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity
 {
-    private Button _tampilMahasiswaButton;
+    private Button _tampilMahasiswaButton,_tampilForexButton;
 
-    private Intent _tampilMahasiswaIntent;
+    private Intent _tampilMahasiswaIntent,_tampilForexIntent;
 
 
     @Override
@@ -20,7 +20,21 @@ public class MenuActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu);
 
         initTampilMahasiswaButton();
+        initTampilForexButton();
     }
+
+    private void initTampilForexButton() {
+        _tampilForexButton = (Button) findViewById(R.id.tampilForexButton);
+
+        _tampilForexButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _tampilForexIntent = new Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexIntent);
+            }
+        });
+    }
+
     private void initTampilMahasiswaButton()
     {
         _tampilMahasiswaButton = (Button) findViewById(R.id.tampilMahasiswaButton);
